18bc11eabec15a9460c0355a5cc61bf5f049b6df
Christian Kirschberg <ckirschberg@gmail.com>
2022-11-01 13:37:56 +0100

    handlebars vs spa

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,19 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
+  // Single page application setup
   //const app = await NestFactory.create(AppModule);
+  
+  
+  // Round trip style application - Handlebars - Start
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
-
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  // Round trip style application - Handlebars - End
 
   
   await app.listen(3005);
