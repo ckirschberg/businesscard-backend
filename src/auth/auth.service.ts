a20ecf32cee14ff906ee75a63c45fb0aada1bc78
Christian Kirschberg <ckirschberg@gmail.com>
2022-11-15 20:25:39 +0100

    got rid of _doc

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/user.schema';
 
 @Injectable()
 export class AuthService {
@@ -10,18 +11,22 @@ export class AuthService {
   ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
+    const user: User = await this.usersService.findOne(username);
+    
     if (user && user.password === pass) {
-      const { password, ...result } = user;
-      return result;
+        // delete user.password;
+        console.log("auth.service", user);    
+        // const { password, ...result } = user; // This does not do what it is supposed to do (remove the password and return the rest of the user object.)
+        console.log("auth.service2 result", user);
+        return user;
     }
     return null;
   }
 
   async login(user: any) {
-    console.log("auth.service user", user._doc);
+    console.log("auth.service user", user);
     
-    const payload = { username: user._doc.username, sub: user._doc._id.toString() };
+    const payload = { username: user.username, sub: user._id.toString() };
     return {
       access_token: this.jwtService.sign(payload),
     };
